cae786af749a5af52eb7666d65f3d39fe62b881a
Richard Jackson <rpjackson2@gmail.com>
2022-03-26 21:28:47 -0500

    working on about us

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
-
-
 // TODO: GSAP STUFF
 const tl = gsap.timeline({defaults:{ease: "power1.out"}});
 
 tl.to(".text",{y:"0", duration:1, stagger:0.25});
 tl.to(".slider",{y:"-100%", duration: 1.5,delay:0.5});
 tl.to(".intro",{y:"-100%",duration:1}, "-=.8");
+
+// TODO: DOM STUFF
+// Global Variables
+
+
+
+// Function Calls
+
+let introSwitcher = document.querySelector(".intro-switcher");
+// Function Definitions
+function changeIntroText() {
+
+    let introText = ["apps","sites","experiences"];
+    introSwitcher.textContent = `${introText[Math.random() * 2]}`
+console.log(introText[2])
+}
+// setInterval(changeIntroText,3500)
