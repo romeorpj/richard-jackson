a15f765b9ff5694d27b97a299918f7e76746981f
Richard Jackson <rpjackson2@gmail.com>
2022-03-28 16:02:38 -0500

    working on animations

--- js/app.js
+++ js/app.js
@@ -10,29 +10,58 @@ tl.to(".intro",{y:"-100%",duration:1}, "-=.8");
 // let imageContainer = document.querySelector(".image-containers");
 // let projectTitle = document.querySelector(".project-title");
 let contactEmail = document.querySelector(".contact-email");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+let mouseCursor = document.querySelector(".cursor");
+let introSwitcher = document.querySelector(".intro-switcher");
 
-// TODO: Function Calls
+let developer = document.querySelector(".developer");
+// TODO: FUNCTION CALLS
+setInterval(changeIntroText,3500)
 
-// let introSwitcher = document.querySelector(".intro-switcher");
-// TODO: Function Definitions
-// function changeIntroText() {
-//
-//     let introText = ["apps","sites","experiences"];
-//     introSwitcher.textContent = `${introText[Math.random() * 2]}`
-// console.log(introText[2])
-// }
-// setInterval(changeIntroText,3500)
-
-async function clickToGetText()
-{
-    await navigator.clipboard.writeText("richard.p.jackson2@gmail.com");
-    console.log("it is clicked")
+// TODO: FUNCTION DEFINITIONS
+// Hero text changer
+function changeIntroText() {
+    let introText = ["apps","sites","experiences"];
+    introSwitcher.textContent = `${introText[Math.floor(Math.random() * 3)]}`
 }
 
+//navbar event
+let animateLinks = ()=>{
+    //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+}
+
+//Copy text to clipboard
+async function clickToGetText()
+{await navigator.clipboard.writeText("richard.p.jackson2@gmail.com");}
+
+//Cursor image follow
+function cursor(e){
+mouseCursor.style.top = `${e.pageY}px`;
+mouseCursor.style.left = `${e.pageX}px`;
+}
+
+//Turn hero text purple
+// function turnLettersPurple(event){
+//     let fullstack = document.querySelector(".fullstack");
+//    return fullstack.split()
+//
+// }
+
+
 // TODO: eventlisteners
-// imageContainer.addEventListener("mouseover",()=>{
-//     projectTitle.classList.toggle()
-// })
 
 //click to copy email
 contactEmail.addEventListener("click", clickToGetText);
+//animates nav links
+hamburger.addEventListener('click', animateLinks);
+//cursor animation
+// window.addEventListener("mousemove", cursor)
+// window.addEventListener('DOMContentLoaded', turnLettersPurple);
