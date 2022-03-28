75212b41370b06ffd83e48c4aee8e138211152b3
Richard Jackson <rpjackson2@gmail.com>
2022-03-27 22:44:28 -0500

    finished mobile design

--- js/app.js
+++ js/app.js
@@ -7,17 +7,32 @@ tl.to(".intro",{y:"-100%",duration:1}, "-=.8");
 
 // TODO: DOM STUFF
 // Global Variables
+// let imageContainer = document.querySelector(".image-containers");
+// let projectTitle = document.querySelector(".project-title");
+let contactEmail = document.querySelector(".contact-email");
 
+// TODO: Function Calls
 
-
-// Function Calls
-
-let introSwitcher = document.querySelector(".intro-switcher");
-// Function Definitions
-function changeIntroText() {
-
-    let introText = ["apps","sites","experiences"];
-    introSwitcher.textContent = `${introText[Math.random() * 2]}`
-console.log(introText[2])
-}
+// let introSwitcher = document.querySelector(".intro-switcher");
+// TODO: Function Definitions
+// function changeIntroText() {
+//
+//     let introText = ["apps","sites","experiences"];
+//     introSwitcher.textContent = `${introText[Math.random() * 2]}`
+// console.log(introText[2])
+// }
 // setInterval(changeIntroText,3500)
+
+async function clickToGetText()
+{
+    await navigator.clipboard.writeText("richard.p.jackson2@gmail.com");
+    console.log("it is clicked")
+}
+
+// TODO: eventlisteners
+// imageContainer.addEventListener("mouseover",()=>{
+//     projectTitle.classList.toggle()
+// })
+
+//click to copy email
+contactEmail.addEventListener("click", clickToGetText);
